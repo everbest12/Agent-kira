301f093807a2db110ff079cfc6b4fb8601cd2c61
[Emmanuel Aniefiok] <[gocitydatascience112@gmail.com]>
2025-04-25 12:35:47 +0100

    increased min password input

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=5, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     submit = SubmitField('Register')
